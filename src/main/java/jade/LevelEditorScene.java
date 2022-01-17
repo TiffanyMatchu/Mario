281ab99084fa5a3e3e06a47d9cb0a30b5bfaeb9b
Tiffany Matthew <8907181+TiffanyMatchu@users.noreply.github.com>
2022-01-17 12:56:42 -0500

    Add scene classes

---
+++ src/main/java/jade/LevelEditorScene.java
@@ -0,0 +1,15 @@
+package jade;
+
+/**
+ * Created by Tiffany Matthew on 1/17/22.
+ */
+public class LevelEditorScene extends Scene {
+    public LevelEditorScene() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
---
+++ src/main/java/jade/LevelScene.java
@@ -0,0 +1,7 @@
+package jade;
+
+/**
+ * Created by Tiffany Matthew on 1/17/22.
+ */
+public class LevelScene {
+}
